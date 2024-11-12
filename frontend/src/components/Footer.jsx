4cdba0fcc55af6b0b3d7032eb45a6b462722b9da
Odiah David <odiahdavid90s@gmail.com>
2024-11-12 07:50:05 -0600

    Project completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -19,17 +19,17 @@ const Footer = () => {
                 <h3 className='text-xl sm:text-xl font-semibold text-primary_text_color items-center space-x-2 flex gap-2 pl-2'>
                   Contact Us
                 </h3>
-                <Link to='https://maps.app.goo.gl/MVQxAbFNNTd27W4XA' className='flex items-center gap-2 hover:text-gray-400 transition ease-in-out duration-300'>
+                <Link to='https://maps.app.goo.gl/7waw5tC9hhagKj6X8' className='flex items-center gap-2 hover:text-gray-400 transition ease-in-out duration-300'>
                 <i className="fa fa-map-marker text-2xl" aria-hidden="true"></i>
-                  5, Akindehinde Str, Isolo, Lagos
+                  Burj Al Arab, Dubai
                 </Link>
-                <Link to='tel:+2348142340393' className='flex items-center gap-2 hover:text-gray-400 transition ease-in-out duration-300'>
+                <Link to='tel:+2348105245417' className='flex items-center gap-2 hover:text-gray-400 transition ease-in-out duration-300'>
                 <i className="fa fa-phone text-2xl" aria-hidden="true"></i>
-                  +234-814-234-0393
+                  +234-810-524-5417
                 </Link>
-                <Link to='mailto:stunner234diamond@gmail.com' className='flex items-center gap-2 hover:text-gray-400 transition ease-in-out duration-300'>
+                <Link to='mailto:davidodiah9@gmail.com' className='flex items-center gap-2 hover:text-gray-400 transition ease-in-out duration-300'>
                 <i className="fa fa-envelope text-2xl" aria-hidden="true"></i>
-                  stunner234diamond@gmail.com
+                  davidodiah9@gmail.com
                 </Link>
             </div>
             <div className='flex flex-col flex-wrap justify-around items-center p-10 gap-6'>
@@ -43,10 +43,10 @@ const Footer = () => {
                   <Link to='https://www.linkedin.com' className=' flex items-center justify-center hover:text-gray-400 transition ease-in-out duration-300 w-8 h-8 rounded-2xl border'>
                     <i className="fa fa-linkedin" aria-hidden="true"></i>
                   </Link>
-                  <Link to='https://www.instagram.com/fireboi363' className=' flex items-center justify-center hover:text-gray-400 transition ease-in-out duration-300 w-8 h-8 rounded-2xl border'>
+                  <Link to='https://www.instagram.com' className=' flex items-center justify-center hover:text-gray-400 transition ease-in-out duration-300 w-8 h-8 rounded-2xl border'>
                     <i className="fa fa-instagram" aria-hidden="true"></i>
                   </Link>
-                  <Link to='https://www.facebook.com/profile.php?id=100088171120693' className=' flex items-center justify-center hover:text-gray-400 transition ease-in-out duration-300 w-8 h-8 rounded-2xl border'>
+                  <Link to='https://www.facebook.com' className=' flex items-center justify-center hover:text-gray-400 transition ease-in-out duration-300 w-8 h-8 rounded-2xl border'>
                     <i className="fa fa-facebook" aria-hidden="true"></i>
                   </Link>
                   <Link to='https://www.youtube.com' className=' flex items-center justify-center hover:text-gray-400 transition ease-in-out duration-300 w-8 h-8 rounded-2xl border'>
